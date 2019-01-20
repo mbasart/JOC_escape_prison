45873efd1a758daf75636535b8188cb93a7f6b5f
dsa0 <dsa0@eetacdsa0.upc.es>
2019-01-20 23:02:28 +0100

    Versión producción

--- src/main/java/DAO/FactorySession.java
+++ src/main/java/DAO/FactorySession.java
@@ -26,8 +26,8 @@ public class FactorySession {
             String dbname = "joc_escape_prison";
             String username = "root";
             //String password = "password";
-            //String password = "Mazinger72";
-            String password = "M1r1tx1ll2797"; //posar contrasenya corresponent
+            String password = "Mazinger72";
+           // String password = "M1r1tx1ll2797"; //posar contrasenya corresponent
             //Class.forName(driver);
             conn =
                     //DriverManager.getConnection("jdbc:mysql://147.83.7.203/"+ dbname + "?user=" + username+ "&password="+password+"");
